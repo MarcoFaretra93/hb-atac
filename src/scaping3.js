cdc45602e03b729724f17ab7bb8ec7031dac141e
Marco Faretra <marcofaretra@MacBook-Pro-di-Marco.local>
2016-09-12 14:49:39 +0200

    agg

--- src/scaping3.js
+++ src/scaping3.js
@@ -20,22 +20,20 @@ var SchedeTecniche = mongoose.model('SchedeTecniche', htmlSchema);
 
 var temp = []
 
-/*file.map(function(value) {
+file.map(function(value) {
 	Object.keys(value.models).map(function(key) {
 		value.models[key].map(function(v) {
 			temp.push('http://www.assicurazione.it'+v.next+ ' ');
 		});
 	});
-});*/
-
-temp = ['http://www.assicurazione.it/audi/a3_20_fsi_attraction_tiptr_3pt.html']
+});
 
 for(element in temp) {
 	var res = request('GET', temp[element]);
 	var entry = new SchedeTecniche({id: temp[element].slice(28).replace('/','_'), html: res.getBody()});
 		entry.save(function(err, data) {
 			if(err) console.log(err);
-			else console.log('fatto');
+			else console.log('fatto'+ element);
 	});
 
 		/*fs.writeFile("/Users/marcofaretra/Documents/scraping_assicurazioni/assicurazione_html/" + temp[element].slice(28).replace('/','_') + ".html", body, function(err) {
